08b09068042258d485fa5bedbf08ec5732af6cec
CraterCreator <jordanfischer13@gmail.com>
2018-09-03 18:28:17 +1000

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator anim;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("Left", false);
+            anim.SetBool("Middle", true);
+        }
+        else
+        {
+            anim.SetBool("Middle", false);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("Left", true);
+        }
+
+        else
+        {
+            anim.SetBool("Left", false);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+        {
+            anim.SetBool("Right", true);
+        }
+
+        else
+        {
+            anim.SetBool("Right", false);
+        }
+    }
+}
