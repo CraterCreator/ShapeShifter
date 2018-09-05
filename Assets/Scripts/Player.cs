5bc00c3a6223a46e19fa0722b3035bfe11eb383d
CraterCreator <jordanfischer13@gmail.com>
2018-09-05 18:15:36 +1000

    Updated Animations - 0.0.2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,9 +14,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.anyKey == false)
+        {
+            anim.SetBool("Middle", false);
+            anim.SetBool("Left", false);
+            anim.SetBool("Right", false);
+            anim.SetBool("R2M", false);
+            anim.SetBool("L2M", false);
+        }
+
         if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow))
         {
-            anim.SetBool("Left", false);
             anim.SetBool("Middle", true);
         }
         else
@@ -24,24 +32,41 @@ public class Player : MonoBehaviour
             anim.SetBool("Middle", false);
         }
 
-        if(Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             anim.SetBool("Left", true);
-        }
 
+            if(Input.GetKey(KeyCode.RightArrow))
+            {
+                anim.SetBool("L2M", true);
+            }
+            else
+            {
+                anim.SetBool("L2M", false);
+            }
+        }
         else
         {
             anim.SetBool("Left", false);
         }
-
-        if(Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+        
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             anim.SetBool("Right", true);
-        }
 
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                anim.SetBool("R2M", true);
+            }
+            else
+            {
+                anim.SetBool("R2M", false);
+            }
+        }
         else
         {
             anim.SetBool("Right", false);
         }
+        
     }
 }
